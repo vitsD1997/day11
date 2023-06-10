9080f6da0a2bcfe7f94687b88603aad933b0e1d6
vitthal <dhakanevitthal30@gmail.com>
2023-06-10 22:38:55 +0530

    done

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
-import { readFile } from "node:fs/promises";
+import { writeFileSync } from "node:fs";
 
 async function main() {
- let filePath ="F:\WPS MODULE\day11\package.json";
- let fileData = await readFile(filePath, {encoding : "utf-8"});
- console.log(fileData);
+let filePath = "F:\WPS MODULE\output1.txt";
+let data = `asynchronously writesdata into a file,
+           replacing the file if it exists. data can be a string, a buffer, an <asyncIterable`
+writeFileSync(filePath, data);
+console.log("write Success!!");
 }
-
-main();
+ main();
