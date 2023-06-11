76eb3fda3b03fb1bf8c7144433fbee2be3f16556
vitthal <dhakanevitthal30@gmail.com>
2023-06-11 05:35:21 +0530

    done

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ let filePath = "F:\WPS MODULE\output1.txt";
 let data = `asynchronously writesdata into a file,
            replacing the file if it already exists. data can be a string, a buffer, an <asyncIterable>,
            or an <Iterable> object.`;
-writeFileSync(filePath, filedata);
+writeFileSync(filePath, data);
 console.log("write Success!!");
 }
 
