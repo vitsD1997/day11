2b10ba49fafd766f4242d92c032e64c96e36ea44
vitthal <dhakanevitthal30@gmail.com>
2023-06-11 05:31:00 +0530

    change

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
-import { writeFileSync } from "node:fs";
+import { writeFileSync } from "node:fs/promises";
 
 async function main() {
 let filePath = "F:\WPS MODULE\output1.txt";
 let data = `asynchronously writesdata into a file,
-           replacing the file if it exists. data can be a string, a buffer, an <asyncIterable`
-writeFileSync(filePath, data);
+           replacing the file if it already exists. data can be a string, a buffer, an <asyncIterable>,
+           or an <Iterable> object.`;
+writeFileSync(filePath, filedata);
 console.log("write Success!!");
 }
+
+
  main();
